e29f3f4bdc874c4ef4eca4f80d92b5ee129c436c
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-25 01:18:57 +0500

    Knight isMovePossible() logic

--- src/components/game.js
+++ src/components/game.js
@@ -130,8 +130,18 @@ class Knight extends Piece {
     super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg"));
   }
 
-  isMovePossible(){
-    return true
+  isMovePossible(src, dest){
+    if(src - 17 === dest || 
+      src - 10 === dest || 
+      src + 6 === dest || 
+      src + 15 === dest || 
+      src - 15 === dest || 
+      src - 6 === dest || 
+      src + 10 === dest || 
+      src + 17 === dest){
+      return true;
+    }
+    return false;
   }
 }
 
