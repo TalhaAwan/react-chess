6aecffb0966346d5d01f639ca4ee05d32618e7d4
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-25 23:45:02 +0500

    pawn isMovePossible() logic

--- src/components/game.js
+++ src/components/game.js
@@ -51,7 +51,7 @@ export default class Game extends React.Component {
       else{
         
         const squares = this.state.squares;
-        console.log("isMovePossible", squares[this.state.sourceSelection].isMovePossible(this.state.sourceSelection, i));
+        console.log("isMovePossible", squares[this.state.sourceSelection].isMovePossible(this.state.sourceSelection, i, true));
         squares[i] = squares[this.state.sourceSelection];
         squares[this.state.sourceSelection] = null;
         let player = this.state.player === 1? 2: 1;
@@ -173,10 +173,31 @@ class King extends Piece {
 class Pawn extends Piece {
   constructor(player){
     super(player, (player === 1?  "https://upload.wikimedia.org/wikipedia/commons/4/45/Chess_plt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/c/c7/Chess_pdt45.svg"));
+    this.initialPositions = {
+      1: [48, 49, 50, 51, 52, 53, 54, 55],
+      2: [8, 9, 10, 11, 12, 13, 14, 15]
+    }
   }
 
-  isMovePossible(){
-    return true
+  isMovePossible(src, dest, enemyOccupied){
+
+    if(this.player === 1){
+      if(dest === src - 8 || (dest === src - 16 && this.initialPositions[1].indexOf(src) !== -1)){
+        return true;
+      }
+      else if(enemyOccupied && (dest === src - 9 || dest === src - 7)){
+        return true;
+      }
+    }
+    else if(this.player === 2){
+      if(dest === src + 8 || (dest === src + 16 && this.initialPositions[2].indexOf(src) !== -1)){
+        return true;
+      }
+      else if(enemyOccupied && (dest === src + 9 || dest === src + 7)){
+        return true;
+      }
+    }
+    return false;
   }
 }
 
