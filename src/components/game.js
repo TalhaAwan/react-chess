bb97c9ff641c6dc64db02b8fe0d7d1d70d1d2bbb
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-27 00:23:08 +0500

    Queen's isMovePossible()

--- src/components/game.js
+++ src/components/game.js
@@ -173,31 +173,6 @@ class Rook extends Piece {
 }
 
 
-class Knight extends Piece {
-  constructor(player){
-    super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg"));
-  }
-
-  isMovePossible(src, dest){
-    return (src - 17 === dest || 
-      src - 10 === dest || 
-      src + 6 === dest || 
-      src + 15 === dest || 
-      src - 15 === dest || 
-      src - 6 === dest || 
-      src + 10 === dest || 
-      src + 17 === dest);
-  }
-
-  /**
-   * always returns empty array because of jumping
-   * @return {[]}
-   */
-  getSrcToDestPath(){
-    return [];
-  }
-}
-
 class Bishop extends Piece {
   constructor(player){
     super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/b/b1/Chess_blt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/9/98/Chess_bdt45.svg"));
@@ -239,20 +214,53 @@ class Bishop extends Piece {
   }
 }
 
-
 class Queen extends Piece {
   constructor(player){
     super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/1/15/Chess_qlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/4/47/Chess_qdt45.svg"));
   }
 
-  isMovePossible(){
-    return true
+  isMovePossible(src, dest){
+    let mod = src % 8;
+    let diff = 8 - mod;
+    
+    return (Math.abs(src - dest) % 9 === 0 || Math.abs(src - dest) % 7 === 0) ||
+      (Math.abs(src - dest) % 8 === 0 || (dest >= (src - mod) && dest < (src + diff)));
   }
 
   getSrcToDestPath(src, dest){
     return [];
   }
 }
+
+
+class Knight extends Piece {
+  constructor(player){
+    super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/7/70/Chess_nlt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/e/ef/Chess_ndt45.svg"));
+  }
+
+  isMovePossible(src, dest){
+    return (src - 17 === dest || 
+      src - 10 === dest || 
+      src + 6 === dest || 
+      src + 15 === dest || 
+      src - 15 === dest || 
+      src - 6 === dest || 
+      src + 10 === dest || 
+      src + 17 === dest);
+  }
+
+  /**
+   * always returns empty array because of jumping
+   * @return {[]}
+   */
+  getSrcToDestPath(){
+    return [];
+  }
+}
+
+
+
+
 
 class King extends Piece {
   constructor(player){
