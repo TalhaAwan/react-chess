34e4f14c0c5bedf7f1b87fb7a49e361c9c424fc0
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-27 00:36:06 +0500

    king's isMovePossible()

--- src/components/game.js
+++ src/components/game.js
@@ -302,8 +302,15 @@ class King extends Piece {
     super(player, (player === 1? "https://upload.wikimedia.org/wikipedia/commons/4/42/Chess_klt45.svg" : "https://upload.wikimedia.org/wikipedia/commons/f/f0/Chess_kdt45.svg"));
   }
 
-  isMovePossible(){
-    return true
+  isMovePossible(src, dest){
+    return (src - 9 === dest || 
+      src - 8 === dest || 
+      src - 7 === dest || 
+      src + 1 === dest || 
+      src + 9 === dest || 
+      src + 8 === dest || 
+      src + 7 === dest || 
+      src - 1 === dest);
   }
 
   getSrcToDestPath(src, dest){
