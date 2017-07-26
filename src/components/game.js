3d3fb82ded4f0a184db7cfa2c3286ddaeb687dbd
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-27 00:16:27 +0500

    Bishop's getSrcToDestPath()

--- src/components/game.js
+++ src/components/game.js
@@ -207,8 +207,35 @@ class Bishop extends Piece {
     return (Math.abs(src - dest) % 9 === 0 || Math.abs(src - dest) % 7 === 0);
   }
 
+    /**
+   * get path between src and dest (src and dest exclusive)
+   * @param  {num} src  
+   * @param  {num} dest 
+   * @return {[array]}      
+   */
   getSrcToDestPath(src, dest){
-    return [];
+    let path = [], pathStart, pathEnd, incrementBy;
+    if(src > dest){
+      pathStart = dest;
+      pathEnd = src;
+    }
+    else{
+      pathStart = src;
+      pathEnd = dest;
+    }
+    if(Math.abs(src - dest) % 9 === 0){
+      incrementBy = 9;
+      pathStart += 9;
+    }
+    else{
+      incrementBy = 7;
+      pathStart += 7;
+    }
+
+    for(let i = pathStart; i < pathEnd; i+=incrementBy){
+      path.push(i);
+    }
+    return path;
   }
 }
 
