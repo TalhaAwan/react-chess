e8a89edc3c679fa5600778cc5cc725e467266326
TalhaAwan <talhamanzoorawan@gmail.com>
2017-07-27 00:26:31 +0500

    Queen's getSrcToDestPath

--- src/components/game.js
+++ src/components/game.js
@@ -182,7 +182,7 @@ class Bishop extends Piece {
     return (Math.abs(src - dest) % 9 === 0 || Math.abs(src - dest) % 7 === 0);
   }
 
-    /**
+  /**
    * get path between src and dest (src and dest exclusive)
    * @param  {num} src  
    * @param  {num} dest 
@@ -227,8 +227,43 @@ class Queen extends Piece {
       (Math.abs(src - dest) % 8 === 0 || (dest >= (src - mod) && dest < (src + diff)));
   }
 
+  /**
+   * get path between src and dest (src and dest exclusive)
+   * @param  {num} src  
+   * @param  {num} dest 
+   * @return {[array]}      
+   */
   getSrcToDestPath(src, dest){
-    return [];
+    let path = [], pathStart, pathEnd, incrementBy;
+    if(src > dest){
+      pathStart = dest;
+      pathEnd = src;
+    }
+    else{
+      pathStart = src;
+      pathEnd = dest;
+    }
+    if(Math.abs(src - dest) % 8 === 0){
+      incrementBy = 8;
+      pathStart += 8;
+    }
+    else if(Math.abs(src - dest) % 9 === 0){
+      incrementBy = 9;
+      pathStart += 9;
+    }
+    else if(Math.abs(src - dest) % 7 === 0){
+      incrementBy = 7;
+      pathStart += 7;
+    }
+    else{
+      incrementBy = 1;
+      pathStart += 1;
+    }
+
+    for(let i = pathStart; i < pathEnd; i+=incrementBy){
+      path.push(i);
+    }
+    return path;
   }
 }
 
