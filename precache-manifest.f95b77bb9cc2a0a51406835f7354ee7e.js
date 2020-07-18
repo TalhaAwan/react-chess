f62386e80bfb6c4b9b3b6b8e0357f46c8c608acd
Talha Awan <talhamanzoorawan@gmail.com>
2020-07-18 16:15:01 +0500

    Updates

---
+++ precache-manifest.f95b77bb9cc2a0a51406835f7354ee7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1ba72e0dc565ce4a9aab3a66ea797c",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "c301fb8f486b918feb1b",
+    "url": "/react-chess/static/css/main.7480f674.chunk.css"
+  },
+  {
+    "revision": "890dfc48d8c1ddfdeefe",
+    "url": "/react-chess/static/js/2.6cc82902.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chess/static/js/2.6cc82902.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c301fb8f486b918feb1b",
+    "url": "/react-chess/static/js/main.bf5f4a4c.chunk.js"
+  },
+  {
+    "revision": "0f0bdfe39706c6ff5a3e",
+    "url": "/react-chess/static/js/runtime-main.c2d716fc.js"
+  }
+]);
